63283b655210c50e690eadb9aaa756ef17a431c5
Abanoub wahied <gfx2day@gmail.com>
2026-01-13 14:02:19 -0700

    add font styles

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  static Color primary = const Color(0xFF171717);
+  static Color secondary = const Color(0xFFFFFFFF);
+
+
+
+}
---
+++ lib/core/resources/styles_manager.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/core/resources/color_manager.dart';
+
+import 'font_manager.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: FontConstants.fontFamily,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+// regular style
+TextStyle getSmallStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s14, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+// regular style
+
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s20, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+// medium style
+
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s24, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+// bold style
+
+TextStyle getBoldStyle({double fontSize = FontSize.s20, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color);
+}
+
+// semibold style
+
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s16, required Color color}) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color );
+}
+
+TextStyle getTextWithLine() {
+  return TextStyle(
+    color: ColorManager.primary,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    decoration: TextDecoration.lineThrough,
+    decorationColor: ColorManager.primary,
+  );
+}
+
